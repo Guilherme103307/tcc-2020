875bdbdab0f35d33f5faa2a1a596f6fd959500b7
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-05-21 09:30:31 -0300

    Update valida.php

--- Controllers/valida.php
+++ Controllers/valida.php
@@ -8,55 +8,69 @@ include_once('conexao.php');
 			$email = $_POST['email'];
 			$senha = $_POST['senha'];
 
-			$query = "SELECT * FROM Usuario WHERE email='$email' and senha='$senha';";
+			$query = "SELECT * FROM Usuario WHERE email='$email' and senha='$senha' LIMIT 1";
 			$verifica = mysqli_query($conexao,$query);
 			$row = mysqli_fetch_assoc($verifica);
 			$tipo = $row['nacionalidade'];
 
-			echo $tipo;
+			
 
 			if ($tipo == "Americano") {
-
+					if(isset($row)){
 				session_start();
 
 				$_SESSION['id']= $tipo;
 
 				
-					$_SESSION['idusuario'] = $row['idusuario'];
+					$_SESSION['idUsuario'] = $row['idUsuario'];
 					$_SESSION['nome'] = $row['nome'];
 					$_SESSION['email'] = $row['email'];
 					$_SESSION['nacionalidade'] = $row['nacionalidade'];
 					$_SESSION['uf'] = $row['uf'];
 					$_SESSION['cidade'] = $row['cidade'];
 					$_SESSION['senha'] = $row['senha'];
+					$_SESSION['NomeIMG'] = $row['NomeImg'];
 
 					header("Location: ../Views/HomeA.php");
-				
+				}
 				
 			} else if ($tipo== "Brasileiro") {
-
+					if(isset($row)){
 					session_start();
 				
-					$_SESSION['idusuario'] = $row['idusuario'];
+					$_SESSION['idUsuario'] = $row['idUsuario'];
 					$_SESSION['nome'] = $row['nome'];
 					$_SESSION['email'] = $row['email'];
 					$_SESSION['nacionalidade'] = $row['nacionalidade'];
 					$_SESSION['uf'] = $row['uf'];
 					$_SESSION['cidade'] = $row['cidade'];
 					$_SESSION['senha'] = $row['senha'];
+					$_SESSION['NomeIMG'] = $row['NomeImg'];
 
 
 					header("Location: ../Views/HomeB.php");
-				}
+				}}
 			
 
 			 else {
 				header("Location: ../Views/Login.php?login=erro");
 			}
-
+				}else{	
+			
+			$_SESSION['loginErro'] = "Usuário ou senha Inválido";
+			header("Location: ../Views/login.php");
 		}
-
-	}
+	
+}	else{
+		$_SESSION['loginErro'] = "Usuário ou senha inválidooo";
+		header("Location: ../Views/login.php");
+	
+}
+		
 
 	
- ?>
+
+
+
+
+?>
