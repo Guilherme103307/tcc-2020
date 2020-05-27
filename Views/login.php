362b5b074e97c4c4f8cf972d476e37bccd872e01
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-05-27 15:21:40 -0300

    Update login.php

--- Views/login.php
+++ Views/login.php
@@ -9,12 +9,23 @@
 	<link rel="stylesheet" type="text/css" href="../css/login.css">
 </head>
 <body>
+	<?php echo $_SESSION['msg'];?>
 	 <div class="login-page">
 	 	<div class="flex-box">
 		<form  method="POST" action="../Controllers/valida.php?acao=logar">
 	 <img src="../Imagens/logo.png" style="width: 8rem; margin-top: 25%">
 			
         	<h2 style="margin-top: 10px; "   >Login</h2>
+        	<div style="bottom: 2%;text-shadow: 2px -2px 10px black; font-size:20px;color: white"><?php
+      if(isset($_SESSION['msg'])){?>
+    
+        <?php
+
+        echo $_SESSION['msg'];
+        unset($_SESSION['msg']);
+
+      }?>
+      </div>
 
         	<div >
 	 		 	<br>
