f634a18370319c50628b98b7e28b0668028ca27d
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-05-27 15:24:39 -0300

    Add files via upload

---
+++ js/personalizado.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a[data-confirm]').click(function(ev){
+		var href = $(this).attr('href');
+		if(!$('#confirm-delete').length){
+			$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header text-white" style="background-color: #FA8072; ">EXCLUIR CONTA<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">Tem certeza de que deseja excluir sua Conta?</div><div class="modal-footer"><button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button><a class="btn text-white" style="background-color: #FA8072; color:white" id="dataComfirmOK">Apagar</a></div></div></div></div>');
+		}
+		$('#dataComfirmOK').attr('href', href);
+        $('#confirm-delete').modal({show: true});
+		return false;
+		
+	});
+});
