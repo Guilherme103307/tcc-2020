7193bfc1ca53c6e60b9518ad373bf2db595aa6de
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-05-27 15:23:08 -0300

    Add files via upload

---
+++ Views/proc_apagar_usuario.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include_once("../Controllers/conexao.php");
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if(!empty($id)){
+	$result_usuario = "DELETE FROM usuario WHERE idUsuario='$id';";
+	$resultado_usuario = mysqli_query($conexao, $result_usuario);
+	if(mysqli_affected_rows($conexao)){
+		$_SESSION['msg'] = "<p style='color:green;'>Usuário apagado com sucesso!!</p>";
+		header("Location: ../views/login.php");
+	}else{
+		
+		$_SESSION['msg'] = "<p style='color:red;'>Erro o usuário não foi apagado com sucesso</p>";
+		header("Location: ../views/conta.php");
+	}
+}else{	
+	$_SESSION['msg'] = "<p style='color:red;'>Necessário selecionar um usuário</p>";
+	header("Location: ../views/conta.php");
+}
