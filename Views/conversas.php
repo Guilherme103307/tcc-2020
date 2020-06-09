04d4e829a2b8c72ae4c7810bfd53bc6b9df55dc7
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-06-09 14:28:42 -0300

    Update conversas.php

--- Views/conversas.php
+++ Views/conversas.php
@@ -1,5 +1,7 @@
 <?php 
 	if (!isset($_SESSION)) session_start();
+  include_once('../Controllers/conexao.php');
+  $Nome = $_SESSION['nome'];
  ?>
 <!DOCTYPE html>
 <html>
@@ -14,6 +16,60 @@
   <body>
   <?php include_once("Header.php") ?>
   
+    <div class="principal"> 
+      <div class="pessoas">
+        <div class="contato2">
+          <div class="pesquisar">
+            <?php 
+              echo "<form class='form-inline my-2 my-lg-0' action='conversas.php' method='POST'>";
+              echo "<input type='search' style='border-bottom: solid; border-top: none; border-right: none; border-left: none; border-width: thin; border-color:  #E8E8E8; margin-left: 15%;' placeholder='Pesquisar' aria-label='Pesquisar' name='pesquisa'>";
+              echo "<button type='submit' value='Pesquisar' style='margin-left: 2%;'>🔎</button>";
+              echo "</form>";
+            ?>
+          </div>
+        </div>
+
+          <?php
+            @$termo = $_POST['pesquisa'];
+            $id = $_SESSION['idUsuario'];
+            $sql_amizade = mysqli_query($conexao, "SELECT * FROM Solicitacoes_de_Amizade WHERE status = 'Aceito' ");
+          while($l_amizade = mysqli_fetch_array($sql_amizade)){
+
+          $id_convite = $l_amizade['idRemetente'];
+          $id_convidado =$l_amizade['idDestinatario'];
+
+          if ($id_convite == $id) {
+           
+          @$Usuario = $id_convidado;
+        }
+        elseif ($id_convidado == $id) {
+          @$Usuario = $id_convite;
+        }
+    
+        
+       $sql_result = mysqli_query($conexao, "SELECT * FROM Usuario WHERE idUsuario like '%$Usuario%' and nome like '%$termo%' ");
+
+          while($line = mysqli_fetch_array($sql_result)){
+            $id_pesquisa = $line['idUsuario'];
+            $nome_pesquisa = $line['nome'];
+            $img_pesquisa = $line['NomeImg'];
+}
+          
+     
+            if (isset($id_pesquisa)){  ?>
+            <div class="contato">
+                <div class="photo"><img src="../foto/<?php echo $img_pesquisa?>" class="photo"></div>
+                <div class="header"> <?php echo $nome_pesquisa?> </div>
+
+            </div>
+
+    <?php }}?>
+   
+      </div>
  
+      <div class="mensagens">
+        teste2
+      </div>
+    </div>
 </body>
 </html>
