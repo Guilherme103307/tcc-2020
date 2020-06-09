d9598a785f70a06864d9446d6c5480766f7575ab
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-06-09 14:35:26 -0300

    Add files via upload

---
+++ js/modal3.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('a[data-confirm]').click(function(ev){
+		var href = $(this).attr('href');
+		if(!$('#confirm-delete').length){
+			$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header text-white" style="background-color: #FA8072; ">Usuario excluido da lista de Amigos!<span aria-hidden="true">&times;</span></button></div><div class="modal-body"></div><div class="modal-footer"><a class="btn text-white" style="background-color: #FA8072; color:white" id="dataComfirmOK">OK!</a></div></div></div></div>');
+		}
+		$('#dataComfirmOK').attr('href', href);
+        $('#confirm-delete').modal({show: true});
+		return false;
+		
+	});
+});
