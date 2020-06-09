47d8ff8adf9e32b7df461315167a2169ca85bae1
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-06-09 15:17:37 -0300

    Update status.php

--- Controllers/status.php
+++ Controllers/status.php
@@ -2,26 +2,27 @@
 	session_start();
 	$id = $_SESSION['idUsuario'];
 	$idR= filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-
 	$status = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_NUMBER_INT);
 
 	
 
-
 	
 	include_once("../Controllers/conexao.php");
-	@$status = $_GET['status'];
 
 	if ($status == '1') {
 		$stts = "Aceito";
+			$sql = mysqli_query($conexao, "UPDATE Solicitacoes_de_Amizade SET status = '$stts' WHERE idDestinatario = '$id' and idRemetente = '$idR' or  idDestinatario = '$idR' and idRemetente ='$id'");
+			mysqli_query($conexao, "DELETE FROM Solicitacoes_de_Amizade WHERE idDestinatario = '$id' and idRemetente = '$idR' and status = 'Pendente'");
+
+	;
 
 	}elseif ($status == '0') {
 		$stts = "Recusado";
+		
+		$sql = "DELETE FROM Solicitacoes_de_Amizade WHERE idDestinatario = '$id' and idRemetente = '$idR' or  idDestinatario = '$idR' and idRemetente ='$id' ";
+		$lala= mysqli_query($conexao, $sql);
+
 	}
-
-	mysqli_query($conexao, "UPDATE Solicitacoes_de_Amizade SET status = '$stts' WHERE idDestinatario = '$id' and idRemetente = '$idR' or  idDestinatario = '$idR' and idRemetente ='$id'");
-
-	mysqli_query($conexao, "DELETE FROM Solicitacoes_de_Amizade WHERE idDestinatario = '$id' and idRemetente = '$idR' and status = 'Pendente'");
 
 if ($status == '0') {
 		$_SESSION['msg'] = "<p style='color:red;'>Solicitação Recusada!!</p>";
