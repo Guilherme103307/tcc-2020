8248397d190b051b42201b3c1dd145d08b8bdd48
Guilherme103307 <62673083+Guilherme103307@users.noreply.github.com>
2020-06-30 11:11:02 -0300

    Update upload.php

--- Controllers/upload.php
+++ Controllers/upload.php
@@ -23,8 +23,81 @@
 			$_UP['erros'][4] = 'Não foi feito o upload do arquivo';
 	
 			if($_FILES['arquivo']['error'] != 0){
-				die("Não foi possivel fazer o upload, erro: <br />". $_UP['erros'][$_FILES['file']['error']]);
-				exit; }
+				$foto='sem_perfil.png';
+			$nome=$_POST['nome'];
+			$email=$_POST['email'];
+			$uf=$_POST['uf'];
+			$cidade=$_POST['cidade'];
+			$senha=$_POST['senha'];
+			$tipo=$_POST['nacionalidade'];
+			$descricao=$_POST['descricao'];
+			
+
+		
+			if ($tipo==1) {
+
+				$nacionalidade = "Americano";
+				$sql="INSERT INTO Usuario (nome, email, senha, uf, cidade, nacionalidade, NomeIMG, descricao) VALUES ('$nome', '$email', '$senha','$uf', '$cidade', '$nacionalidade', '$foto', '$descricao');";
+				
+				$lala= mysqli_query($conexao, $sql);
+				
+				
+				$query = "SELECT * FROM Usuario WHERE email='$email' and senha='$senha';";
+				$verifica = mysqli_query($conexao,$query);
+				$row = mysqli_fetch_assoc($verifica);
+				$tipo = $row['nacionalidade'];
+
+				session_start();
+
+				$_SESSION['id']= $tipo;
+
+					
+					$_SESSION['idUsuario'] = $row['idUsuario'];
+					$_SESSION['nome'] = $nome;
+					$_SESSION['email'] = $email;
+					$_SESSION['nacionalidade'] = $nacionalidade;
+					$_SESSION['uf'] = $uf;
+					$_SESSION['cidade'] = $cidade;
+					$_SESSION['senha'] = $senha;
+					$_SESSION['NomeIMG'] = $foto;
+					$_SESSION['descricao'] = $descricao;
+
+			
+						header("Location:../Views/homeB.php");
+			}
+
+			if ($tipo==2) {
+
+				$nacionalidade = "Brasileiro";
+					$sql="INSERT INTO Usuario (nome, email, senha, uf, cidade, nacionalidade, NomeIMG, descricao) VALUES ('$nome', '$email', '$senha','$uf', '$cidade', '$nacionalidade', '$foto', '$descricao');";
+				$lala= mysqli_query($conexao, $sql);
+				
+				$query = "SELECT * FROM Usuario WHERE email='$email' and senha='$senha';";
+				$verifica = mysqli_query($conexao,$query);
+				$row = mysqli_fetch_assoc($verifica);
+				$tipo = $row['nacionalidade'];
+
+				session_start();
+
+				$_SESSION['id']= $tipo;
+
+				
+					$_SESSION['idUsuario'] = $row['idUsuario'];
+					$_SESSION['nome'] = $nome;
+					$_SESSION['email'] = $email;
+					$_SESSION['nacionalidade'] = $nacionalidade;
+					$_SESSION['uf'] = $uf;
+					$_SESSION['cidade'] = $cidade;
+					$_SESSION['senha'] = $senha;
+					$_SESSION['NomeIMG'] = $foto;
+					$_SESSION['descricao'] = $descricao;
+					
+			
+				header("Location:../Views/homeB.php");
+
+			}
+		}
+				
 			
 			else if ($_UP['tamanho'] < $_FILES['arquivo']['size']){
 				echo "
